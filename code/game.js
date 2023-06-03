4a7a6d8e775ad0cc6019e25d309bfba2b42f5fb6
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-03 14:47:38 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -2,15 +2,21 @@ scene('game', () => {
   const mark = add([
     sprite('mark'),
     pos(SCALE*3, SCALE*3),
-    scale((SCALE/640) * 3), // SCALE divided by sprite width, multiplied by the size change
+    scale(SCALE/640 * 2), // SCALE divided by sprite width, multiplied by the size change
     rotate(0),
     anchor('center'),
-
+  ]);
+  
+  const player = add([
+    sprite('player'),
+    pos(0,0),
+    scale(SCALE/64 * 0.5),
+    anchor('center')
   ]);
 
   onUpdate(() => {
     mark.angle += dt()*150;
-    mark.pos = mousePos();
+    player.pos = mousePos();
   });
 });
 
