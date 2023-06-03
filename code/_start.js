effc024b5900d27dcdd627f7965be044ee794ef5
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-03 11:53:27 -0400

    Initial commit

---
+++ code/_start.js
@@ -0,0 +1,28 @@
+const SCREEN_RATIO = 0.6;
+
+let ww = window.innerWidth;let wh = window.innerHeight;let kaboomDimensions = {};if (ww * SCREEN_RATIO > wh) {kaboomDimensions = { w: wh / SCREEN_RATIO,h: wh};} else {kaboomDimensions = {w: ww,h: ww * SCREEN_RATIO};};
+
+kaboom({
+  background: [255,255,255],
+  width: kaboomDimensions.w,
+  height: kaboomDimensions.h,
+  inspectColor: [255,255,255],
+  pixelDensity: 1,
+  crisp: true,
+});
+
+debug.inspect = false;
+
+function ls(a,b) {
+  if (b == undefined) {
+    loadSprite(a, `${a}.png`); 
+  } else {
+    loadSprite(a, `${a}.png`, b);
+  };
+};
+
+const SCALE = width()/16;
+
+loadRoot('sprites/');
+
+ls('apple');
