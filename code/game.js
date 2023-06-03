92934951a30c816c59ffc2ff78388f1596ee9142
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-03 14:26:31 -0400

    markified

--- code/game.js
+++ code/game.js
@@ -1,16 +1,16 @@
 scene('game', () => {
-  const apple = add([
-    sprite('apple'),
+  const mark = add([
+    sprite('mark'),
     pos(SCALE*3, SCALE*3),
-    scale((SCALE/64) * 3), // SCALE divided by sprite width, multiplied by the size change
+    scale((SCALE/640) * 3), // SCALE divided by sprite width, multiplied by the size change
     rotate(0),
     anchor('center'),
 
   ]);
 
   onUpdate(() => {
-    apple.angle += dt()*150;
-    apple.pos = mousePos();
+    mark.angle += dt()*150;
+    mark.pos = mousePos();
   });
 });
 
