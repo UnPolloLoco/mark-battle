de3e310698a67545e0429c61ae944cd2e8f2ced0
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-03 20:30:34 -0400

    dark mode and no more crisp

--- code/_start.js
+++ code/_start.js
@@ -3,12 +3,12 @@ const SCREEN_RATIO = 0.6;
 let ww = window.innerWidth;let wh = window.innerHeight;let kaboomDimensions = {};if (ww * SCREEN_RATIO > wh) {kaboomDimensions = { w: wh / SCREEN_RATIO,h: wh};} else {kaboomDimensions = {w: ww,h: ww * SCREEN_RATIO};};
 
 kaboom({
-  background: [255,255,255],
+  background: [50,50,50],
   width: kaboomDimensions.w,
   height: kaboomDimensions.h,
   inspectColor: [255,255,255],
   pixelDensity: 1,
-  crisp: true,
+  crisp: false,
 });
 
 debug.inspect = false;
