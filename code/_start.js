0f50bbc1a4aa22d70d981d1dfc06f01ecfa1ece5
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-06 11:10:38 -0400

    very slightly brighter bg

--- code/_start.js
+++ code/_start.js
@@ -3,7 +3,7 @@ const SCREEN_RATIO = 0.6;
 let ww = window.innerWidth;let wh = window.innerHeight;let kaboomDimensions = {};if (ww * SCREEN_RATIO > wh) {kaboomDimensions = { w: wh / SCREEN_RATIO,h: wh};} else {kaboomDimensions = {w: ww,h: ww * SCREEN_RATIO};};
 
 kaboom({
-  background: [30,30,30],
+  background: [32,32,32],
   width: kaboomDimensions.w,
   height: kaboomDimensions.h,
   inspectColor: [255,255,255],
