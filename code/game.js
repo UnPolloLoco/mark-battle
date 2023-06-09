8423fbbda05a1ad029958fcd674c738494bab991
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-09 17:32:53 -0400

    slash area offset

--- code/game.js
+++ code/game.js
@@ -63,7 +63,10 @@ scene('game', () => {
     pos(0, -150),
     anchor('center'),
     scale(vec2(16, 10)),
-    area({ scale: vec2(0.95, 0.22) }),
+    area({ 
+      scale: vec2(0.95, 0.22),
+      offset: vec2(0, 20),
+    }),
     opacity(0),
     shader('slash'),
   ]);
