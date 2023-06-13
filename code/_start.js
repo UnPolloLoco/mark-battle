0676ff15b7680b8ed572778bcc793e28a1b5f7de
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-06-13 08:02:10 -0400

    255*3

--- code/_start.js
+++ code/_start.js
@@ -80,7 +80,7 @@ loadShader('light', null, `
   }
 `);
 
-loadShader('slash', null, `
+loadShader('white', null, `
   vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
     vec4 c = def_frag();
     return vec4(1, 1, 1, c.a);
