23eb193466b291be4530fe5bfeab5db2f3debde5
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-07-13 07:00:11 -0400

    epic logic??

--- code/game.js
+++ code/game.js
@@ -606,7 +606,7 @@ scene('game', () => {
           m.xVel -= m.xVel * dt() * (
             m.isGrounded() ? GROUND_FRICTION : AIR_FRICTION
           );
-          if (player.pos.y > SCALE*4.3 && Math.abs(player.pos.y - m.pos.y) > SCALE*0.65) {
+          if ((player.pos.y > SCALE*4.3 || m.pos.y > SCALE*4.5) && Math.abs(player.pos.y - m.pos.y) > SCALE*0.65) {
             m.forceMove = rand() < 0.5 ? 'left' : 'right';
           };
         };
