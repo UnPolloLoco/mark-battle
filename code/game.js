d2269dd5086ee37b2fc4b941cdad18e5c983de19
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-07-25 13:14:06 -0400

    would it be scienceing or sciencing?

--- code/game.js
+++ code/game.js
@@ -147,7 +147,7 @@ scene('game', () => {
     sprite('slash'),
     pos(500, -150), // 0, -150
     anchor('center'),
-    scale(vec2(4, 2.5)), // vec2(16, 10)
+    scale(vec2(2, 2.5)), // vec2(16, 10)
     area({ 
       scale: vec2(0.95, 0.22),
       offset: vec2(0, 90),
