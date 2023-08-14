934875dfa37317d71b4d4328eb7113fd8a941e8a
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-08-13 21:19:16 -0400

    IT WORKS

--- code/game.js
+++ code/game.js
@@ -626,7 +626,6 @@ scene('game', () => {
 		  	player.jump(JUMP_SPEED);
 	  	};
     };
-    debug.log(`\n ${fixedMousePos < touchJumpCheck - SCALE*2} \n fmp ${fixedMousePos} \n tjc ${touchJumpCheck}`);
     touchJumpCheck = fixedMousePos;
   });
   
