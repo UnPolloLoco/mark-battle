708e4ee4c296a8681abd38f5b28f91c2ba907214
UnPolloLoco <128266839+UnPolloLoco@users.noreply.github.com>
2023-09-25 17:49:18 -0400

    fail screen fade in

--- code/failScreen.js
+++ code/failScreen.js
@@ -1,14 +1,24 @@
 scene('fail', () => {
   setBackground(BLACK);
   usePostEffect('clear');
+
+  add([
+    rect(width(), height()),
+    pos(0,0),
+    color(BLACK),
+    lifespan(0, { fade: 0.6 }),
+    opacity(1),
+    z(10),
+  ])
   
   add([
     text('WALLOPED', { 
       size: SCALE,
       font: 'playfair',
     }),
-    pos(center()),
+    pos(width()/2, SCALE*4),
     color(WHITE),
     anchor('center'),
+    z(0),
   ]);
 });
