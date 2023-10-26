a97beb21b2425f29f76e3f8b134c1b01b1ef02e7
Luka <128266839+UnPolloLoco@users.noreply.github.com>
2023-10-26 09:39:45 -0400

    jfoiavjsheariwfklmsdj

--- code/game.js
+++ code/game.js
@@ -1675,7 +1675,7 @@ scene('game', () => {
   };
   // end of un-paused checker thingy
 
-  debug.log((timeReal() - player.timeOfDeath))
+  debug.log(`${timeReal() - player.timeOfDeath}`);
     
   });
 });
