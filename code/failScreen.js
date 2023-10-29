9d30422f9bca02cc11ab11f01322447513893814
Luka <128266839+UnPolloLoco@users.noreply.github.com>
2023-10-29 18:14:20 -0400

    starting the new fail screen layout

--- code/failScreen.js
+++ code/failScreen.js
@@ -12,78 +12,40 @@ scene('fail', () => {
   	})),
     z(-1),
   ]);
-
-
-  // main text
-  /*add([
-    text('bombastically', { 
-      size: SCALE/2,
-      font: 'reenieBeanie',
-    }),
-    pos(SCALE*2.6, SCALE*1.3),
-    color(rgb(150,150,150)),
-    anchor('center'),
-    rotate(-10),
-    z(1),
-  ]);*/
   
   add([
     text('WALLOPED', { 
-      size: SCALE * 1.4,
+      size: SCALE,
       font: 'nerko',
     }),
-    pos(width()/2, SCALE*1.7), // prev 2
+    pos(width()/2, SCALE*3),
     color(WHITE),
-    anchor('center'),
+    anchor('bot'),
     z(1),
   ]);
 
   // scoring
 
-  add([
-    text('Score: 1234', {
-      size: SCALE/2,
-      font: 'reenieBeanie',
-    }),
-    pos(SCALE*2, SCALE*3),
-    anchor('left'),
-    z(1)
-  ]);
-
-  add([
-    text('Completion: 123%', {
-      size: SCALE/2,
-      font: 'reenieBeanie',
-    }),
-    pos(width()/2, SCALE*3),
-    anchor('left'),
-    z(1)
-  ]);
-
-  // buttons
-
   for (let i = 0; i < 2; i++) {
-    let rectWidth = (i == 0) ? 6 : 0.25;
-    let rectOpacity = (i == 0) ? 0.2 : 1;
-
-    rectWidth *= SCALE;
+    let scorerText = ['1234 points', '56% complete'][i];
+    let yPosAdd = [SCALE/2, SCALE][i];
     
     add([
-      rect(rectWidth, SCALE*0.5),
-      pos(SCALE*2, SCALE*3.5),
-      color(GREEN),
-      opacity(rectOpacity),
-      z(0),
-    ]);
-  
-    add([
-      rect(rectWidth, SCALE*0.5),
-      pos(SCALE*2, SCALE*4.5),
-      color(RED),
-      opacity(rectOpacity),
-      z(0),
+      text(scorerText, {
+        size: SCALE/4,
+        font: 'reenieBeanie',
+        align: 'right'
+      }),
+      pos(
+        width()/2 - SCALE/2, 
+        height()/2 + yPosAdd
+      ),
+      anchor('right'),
+      z(1),
+      color(rgb(240, 195, 255)),
     ]);
   };
+
 
   
 
