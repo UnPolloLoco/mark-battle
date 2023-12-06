06628005c624023ab4ee278422b67bc3df8ceccf
Luka <128266839+UnPolloLoco@users.noreply.github.com>
2023-12-06 17:33:02 -0500

    red overlay squared

--- code/_shaders.js
+++ code/_shaders.js
@@ -202,6 +202,8 @@ loadShader('redOverlay', null, `
     vec4 c = def_frag();
 	
     float dist = distance(pos, vec2(0, 0));
+    dist *= dist;
+    
     float alpha = smoothstep(0.0, 1.0,
       dist * 0.25 + 0.07
     );
